d57a41f0ca878b3557f8ae76577fc2a5b9105542
Ariel Alvarez <arielalvarez88@gmail.com>
2011-06-24 17:09:44 -0400

    Probando

--- sites/all/themes/basic/page.tpl.php
+++ sites/all/themes/basic/page.tpl.php
@@ -6,7 +6,7 @@
         <title>5050MKT Agencia Creativa</title>
         <?php print $head; ?>
         <?php print $styles; ?>
-
+        <?php var_dump('hola');?>
 <!--        <script type="text/javascript" src="http://use.typekit.com/hxo2xpq.js"></script>
 <script type="text/javascript">try{Typekit.load();}catch(e){}</script>-->
 <!--[if lte IE 6]><style type="text/css" media="all">@import "<?php print $base_path . path_to_theme() ?>/css/ie6.css"</style><![endif]-->
