c55ff8da8ff4e51804384aa6ae1d2b9a48671ee6
Ariel Alvarez <arielalvarez88@gmail.com>
2011-06-27 19:06:50 -0400

    Adding contacto-mensaje ajax

---
+++ includes/ajax/contacto-mensaje.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$subject = "Prueba";
+$to = 'contact@5050mkt.com';
+$body = 'hola eto e una prudeba';
+$response = new stdClass();
+$response->header = 'Error';
+    $response->body = 'Ha ocurrido un error, inténtelo más tarde';
+if(mail($subject,$to,$body))
+{
+    
+    $response->header = 'Confirmación';
+    $response->body = 'Su mensaje fue enviado';
+    echo json_encode($response);
+}
+
+echo json_encode($response);
+
+?>
