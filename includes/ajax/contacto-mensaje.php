9a6a3082a1f33c3e854441c6f8ac463b98974cf5
Ariel Alvarez <arielalvarez88@gmail.com>
2011-06-27 19:30:03 -0400

    scaping strings with different function

--- includes/ajax/contacto-mensaje.php
+++ includes/ajax/contacto-mensaje.php
@@ -5,10 +5,10 @@
  * and open the template in the editor.
  */
 
-$nombre = mysqli_real_escape_string($_POST['nombre']);
-$email = mysqli_real_escape_string($_POST['email']);
-$empresa = mysqli_real_escape_string($_POST['empresa']);
-$mensasje = mysqli_real_escape_string($_POST['mensaje']);
+$nombre = mysql_escape_mimic($_POST['nombre']);
+$email = mysql_escape_mimic($_POST['email']);
+$empresa = mysql_escape_mimic($_POST['empresa']);
+$mensasje = mysql_escape_mimic($_POST['mensaje']);
 $to = 'contacto@5050mkt.com';
 $response = new stdClass();
 $response->header = 'Error';
@@ -22,5 +22,17 @@ if(mail($to,$nombre.'-'.$empresa,$mensaje))
 }
 
 echo json_encode($response);
+
+
+function mysql_escape_mimic($inp) {
+    if(is_array($inp))
+        return array_map(__METHOD__, $inp);
+
+    if(!empty($inp) && is_string($inp)) {
+        return str_replace(array('\\', "\0", "\n", "\r", "'", '"', "\x1a"), array('\\\\', '\\0', '\\n', '\\r', "\\'", '\\"', '\\Z'), $inp);
+    }
+
+    return $inp;
+} 
 
 ?>
