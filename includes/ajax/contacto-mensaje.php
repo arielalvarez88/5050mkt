1ceaba3796479e13c9fc0f9fdc77cc03440d6455
Ariel Alvarez <arielalvarez88@gmail.com>
2011-06-29 10:26:56 -0400

    Fixing email and date

--- includes/ajax/contacto-mensaje.php
+++ includes/ajax/contacto-mensaje.php
@@ -4,13 +4,16 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+require_once dirname(__FILE__).'/../repositories/DrupalRepository.php';
+$repository = new DrupalRepository();
+$nombre = $repository->escapeString($_POST['nombre']);
+$email = $repository->escapeString($_POST['email']);
+$empresa = $repository->escapeString($_POST['empresa']);
+$mensaje = $repository->escapeString($_POST['mensaje']);
 
-$nombre = mysql_escape_mimic($_POST['nombre']);
-$email = mysql_escape_mimic($_POST['email']);
-$empresa = mysql_escape_mimic($_POST['empresa']);
-$mensasje = mysql_escape_mimic($_POST['mensaje']);
+
 $to = 'contacto@5050mkt.com';
-$response = new stdClass();
+$response = new stdtClass();
 $response->header = 'Error';
     $response->body = 'Ha ocurrido un error, inténtelo más tarde';
 if(mail($to,$nombre.'-'.$empresa,$mensaje))
