ed2039b3f0dfdbd02127334b71f617f95d3c1b01
unknown <Ariel@.(none)>
2011-10-22 13:36:54 -0400

    adding / to the inputs tags

--- includes/blocks/newsLetter.php
+++ includes/blocks/newsLetter.php
@@ -2,7 +2,7 @@
                         <span class="museo-font">Newsletter</span>
                         <p class="museo-font">Recibe ofertas e informaciones sobre nuestros servicios</p>
                         <div id="newsletter-email-container" class="round-corners">
-                            <input type="text" class="round-corners" id="newsletter-input"> 
+                            <input type="text" class="round-corners" id="newsletter-input"/> 
                             <a id="newsletter-subscribe-button" class="no-decoration-anchor round-corners" href="#javascript">Suscríbete</a>
                         </div>
                         
