75f02c8d8418bffa23c006ffad3e6b1229b6d091
Ariel <ariel@ariel-desktop.(none)>
2012-01-11 16:25:05 -0400

    google analytics

--- sites/all/themes/basic/page.tpl.php
+++ sites/all/themes/basic/page.tpl.php
@@ -193,6 +193,21 @@
 
 
         </div> <!-- /page -->
+        
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-28271960-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
         <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
         <script type="text/javascript" src="http://use.typekit.com/hxo2xpq.js"></script>
 <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
